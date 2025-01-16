5c564667e999b864f91fc1c9cc1eec5d411ae438
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2025-01-16 14:00:02 +0100

    init: complete the task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const output = fs.createWriteStream(path.join(__dirname, 'output.txt'));
+const { stdout, stdin, exit } = process;
+function byeAndExit() {
+  stdout.write('Thank you! bye-bye\n');
+  exit();
+}
+stdout.write('Please type something!\n');
+stdin.on('data', (data) => {
+  const str = data.toString();
+  if (str.trim() === 'exit') {
+    byeAndExit();
+  }
+  output.write(str);
+});
+process.on('SIGINT', byeAndExit);
